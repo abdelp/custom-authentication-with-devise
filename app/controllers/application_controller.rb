0526a777b1cb0371cf890091d0619149ad9c84ae
abdelp <juniorperezpy@gmail.com>
2020-11-25 20:33:25 -0300

    Add User controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username avatar])
   end
 end
